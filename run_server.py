f674142d8bd3dcb364c21e7cfd560e144057546d
Nikolay021978 <158850375+Nikolay021978@users.noreply.github.com>
2025-12-31 20:09:42 +0300

    Add files via upload

--- run_server.py
+++ run_server.py
@@ -26,9 +26,9 @@ from PIL import Image, ImageOps
 # ---------------------------
 # Configuration
 # ---------------------------
-TURN_TIME_QUIZ = 40 # время на ответ для формата quiz (в секундах)
+TURN_TIME_QUIZ = 10 # время на ответ для формата quiz (в секундах)
 ROUND_DELAY = 0.8 # задержка между турами (в секундах)
-NUM_QUESTIONS = 5 # число вопросов в раунде
+NUM_QUESTIONS = 8 # число вопросов в раунде
 
 THUMB_SIZE = "200x200" # размер миниатюр
 
